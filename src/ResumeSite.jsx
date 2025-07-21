26a191cf541788e62373122f2086e117dc971c9c
Mikey <stenli.lolo@gmail.com>
2025-07-21 13:44:07 +0200

    Fix: исправлен импорт изображения профиля

--- src/ResumeSite.jsx
+++ src/ResumeSite.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import profileImage from "./image2/png_resume.jpg";
 
 export default function ResumeSite() {
     return (
         <div className="container">
             <header>
-                <img src="/image2/png_resume.jpg" alt="Фото Владислава" className="profile-pic" />
+                <img src={profileImage} alt="Фото Владислава" className="profile-pic" />
                 <h1>Morhachov Vladyslav</h1>
                 <p className="subtitle">Front-End Developer | Freelancer on Fiver and UpWork</p>
             </header>
